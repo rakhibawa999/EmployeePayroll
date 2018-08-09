786264a437e849622b91b5592a401ac6b99c7f15
MacStudent <macstudent@20x-imacxx-m-469.local>
2018-08-09 17:14:23 -0400

    project first commit

--- EmployeePayroll/SplashViewController.swift
+++ EmployeePayroll/SplashViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class SplashViewController: UIViewController {
+    
+    override func viewDidLoad()
+        
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        perform(#selector(self.goToLoginScreen), with: nil, afterDelay: 5)
+        
     }
-
+    
+    @objc func goToLoginScreen()
+        
+    {
+        performSegue(withIdentifier: "showLoginScreen", sender: self)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
